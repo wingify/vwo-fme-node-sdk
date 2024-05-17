61733576e5caa4700ad9a4f1e785649366464716
softvar <varun2902@gmail.com>
2024-05-18 03:32:06 +0530

    chore(util): add comments in the files

--- lib/utils/ImpressionUtil.ts
+++ lib/utils/ImpressionUtil.ts
@@ -1,21 +1,52 @@
+/**
+ * Copyright 2024 Wingify Software Pvt. Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import { SettingsModel } from "../models/settings/SettingsModel";
 import { NetworkUtil } from "./NetworkUtil";
 import { ContextModel } from "../models/user/ContextModel";
 import { EventEnum } from "../enums/EventEnum";
 
+/**
+ * Creates and sends an impression for a variation shown event.
+ * This function constructs the necessary properties and payload for the event
+ * and uses the NetworkUtil to send a POST API request.
+ *
+ * @param {SettingsModel} settings - The settings model containing configuration.
+ * @param {number} campaignId - The ID of the campaign.
+ * @param {number} variationId - The ID of the variation shown to the user.
+ * @param {ContextModel} context - The user context model containing user-specific data.
+ */
 export const createAndSendImpressionForVariationShown = (
   settings: SettingsModel,
   campaignId: number,
   variationId: number,
   context: ContextModel,
 ) => {
+  // Initialize network utility
   const networkUtil = new NetworkUtil();
+
+  // Get base properties for the event
   const properties = networkUtil.getEventsBaseProperties(
     settings,
     EventEnum.VWO_VARIATION_SHOWN,
-    encodeURIComponent(context.getUserAgent()),
+    encodeURIComponent(context.getUserAgent()), // Encode user agent to ensure URL safety
     context.getIpAddress(),
   );
+
+  // Construct payload data for tracking the user
   const payload = networkUtil.getTrackUserPayloadData(
     settings,
     context.getId(),
@@ -26,5 +57,6 @@ export const createAndSendImpressionForVariationShown = (
     context.getIpAddress(),
   );
 
+  // Send the constructed properties and payload as a POST request
   networkUtil.sendPostApiRequest(properties, payload);
 };
--- lib/utils/RuleEvaluationUtil.ts
+++ lib/utils/RuleEvaluationUtil.ts
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2024 Wingify Software Pvt. Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import { CampaignModel } from "../models/campaign/CampaignModel";
 import { FeatureModel } from "../models/campaign/FeatureModel";
 import { SettingsModel } from "../models/settings/SettingsModel";
@@ -8,10 +23,20 @@ import { checkWhitelistingAndPreSeg } from "./DecisionUtil";
 import { createAndSendImpressionForVariationShown } from "./ImpressionUtil";
 
 /**
- * Evaluate the rule
- * @param rule    rule to evaluate
- * @param user    user object
- * @returns
+ * Evaluates the rules for a given campaign and feature based on the provided context.
+ * This function checks for whitelisting and pre-segmentation conditions, and if applicable,
+ * sends an impression for the variation shown.
+ *
+ * @param {SettingsModel} settings - The settings configuration for the evaluation.
+ * @param {FeatureModel} feature - The feature being evaluated.
+ * @param {CampaignModel} campaign - The campaign associated with the feature.
+ * @param {ContextModel} context - The user context for evaluation.
+ * @param {Map<string, any>} evaluatedFeatureMap - A map of evaluated features.
+ * @param {Map<number, number>} megGroupWinnerCampaigns - A map of MEG group winner campaigns.
+ * @param {StorageService} storageService - The storage service for persistence.
+ * @param {any} decision - The decision object that will be updated based on the evaluation.
+ * @returns {Promise<[boolean, any]>} A promise that resolves to a tuple containing the result of the pre-segmentation
+ * and the whitelisted object, if any.
  */
 export const evaluateRule = async (
   settings: SettingsModel,
@@ -23,7 +48,7 @@ export const evaluateRule = async (
   storageService: StorageService,
   decision: any,
 ): Promise<[boolean, any]> => {
-  // check for whitelisting and pre segmentation
+  // Perform whitelisting and pre-segmentation checks
   const [preSegmentationResult, whitelistedObject] = await checkWhitelistingAndPreSeg(
     settings,
     feature,
@@ -35,14 +60,16 @@ export const evaluateRule = async (
     decision,
   );
 
-  // if pre segmentation result is true and whitelisted object is present, then send post call
+  // If pre-segmentation is successful and a whitelisted object exists, proceed to send an impression
   if (preSegmentationResult && isObject(whitelistedObject) && Object.keys(whitelistedObject).length > 0) {
+    // Update the decision object with campaign and variation details
     Object.assign(decision, {
       experimentId: campaign.getId(),
       experimentKey: campaign.getKey(),
       experimentVariationId: whitelistedObject.variationId,
     });
 
+    // Send an impression for the variation shown
     createAndSendImpressionForVariationShown(
       settings,
       campaign.getId(),
@@ -51,5 +78,6 @@ export const evaluateRule = async (
     );
   }
 
+  // Return the results of the evaluation
   return [ preSegmentationResult, whitelistedObject ];
 };

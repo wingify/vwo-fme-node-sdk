2b0a5fa29b74077695742dce93475752afac3290
softvar <varun2902@gmail.com>
2024-05-22 18:19:02 +0530

    refactor: use log-messages repo instead of harcoding messages in repo

--- lib/enums/log-messages/index.ts
+++ lib/enums/log-messages/index.ts
@@ -1,5 +1,7 @@
-import { DebugLogMessagesEnum } from './DebugLogMessagesEnum';
-import { InfoLogMessagesEnum } from './InfoLogMessagesEnum';
-import { ErrorLogMessagesEnum } from './ErrorLogMessagesEnum';
+import * as messages from 'vwo-fme-sdk-log-messages';
+
+const DebugLogMessagesEnum = messages.debugMessages;
+const InfoLogMessagesEnum = messages.infoMessages;
+const ErrorLogMessagesEnum = messages.errorMessages;
 
 export { DebugLogMessagesEnum, InfoLogMessagesEnum, ErrorLogMessagesEnum };
